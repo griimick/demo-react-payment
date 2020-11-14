b5c241d573f86b16374f044eef4ab750e4994610
Soumik Pradhan <soumikpradhan14@gmail.com>
2020-11-14 23:14:06 +0530

    Fix wrong match

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,12 @@ import { Navbar, NavbarBrand, NavLink } from "reactstrap";
 import { useRouteMatch } from "react-router-dom";
 
 export default function Header() {
-  let matchLogin = useRouteMatch("/blog/:slug");
+  let matchLogin = useRouteMatch("/login");
   return (
     <div>
       <Navbar color="dark" dark expand="md">
         <NavbarBrand href="/">asd</NavbarBrand>
-        {matchLogin && (
+        {!matchLogin && (
           <NavLink style={{ float: "right" }} href="/login">
             Log In
           </NavLink>
