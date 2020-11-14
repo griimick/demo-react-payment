e18297309f3912951673833e1b7051ca503406e0
Soumik Pradhan <soumikpradhan14@gmail.com>
2020-11-14 23:13:08 +0530

    Remove Login link from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { Navbar, NavbarBrand, NavLink } from "reactstrap";
+import { useRouteMatch } from "react-router-dom";
 
 export default function Header() {
+  let matchLogin = useRouteMatch("/blog/:slug");
   return (
     <div>
       <Navbar color="dark" dark expand="md">
         <NavbarBrand href="/">asd</NavbarBrand>
-        <NavLink style={{ float: "right" }} href="/login">
-          Log In
-        </NavLink>
+        {matchLogin && (
+          <NavLink style={{ float: "right" }} href="/login">
+            Log In
+          </NavLink>
+        )}
       </Navbar>
     </div>
   );
